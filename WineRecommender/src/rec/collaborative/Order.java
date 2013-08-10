63f5e6a00ff88010c2c365f91f687fd18941cfc3
Lukas Reuter <reuter-lukas@web.de>
2013-08-10 13:39:34 +0200

    neue Klassen

--- WineRecommender/src/rec/collaborative/Order.java
+++ WineRecommender/src/rec/collaborative/Order.java
@@ -1,5 +1,26 @@
 package rec.collaborative;
 
+import java.util.Vector;
+
 public class Order {
 
+	private int orderId;
+	private int similairity;
+	Vector<Integer> wine = new Vector();
+
+	public int getSimilairity() {
+		return similairity;
+	}
+
+	public void setSimilairity(int similairity) {
+		this.similairity = similairity;
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
 }
