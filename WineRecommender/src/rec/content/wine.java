0dff09c68e784bdbe2047c8ef2f75f77d1e92224
Zolitschka <zolitschka@googlemail.com>
2013-08-10 11:59:53 +0200

    wine-klasse hinzugefügt

---
+++ WineRecommender/src/rec/content/wine.java
@@ -0,0 +1,117 @@
+package rec.content;
+
+public class wine {
+	private double price;
+	private int winery;
+	private int quality;
+	private int region;
+	private int taste;
+	private double alcohol;
+	private double acid;
+	private double sweetness;
+	private int vdp;
+	private int grape[];
+	private int wineStyle;
+	private int year;
+
+	public wine() {
+		
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getWinery() {
+		return winery;
+	}
+
+	public void setWinery(int winery) {
+		this.winery = winery;
+	}
+
+	public int getQuality() {
+		return quality;
+	}
+
+	public void setQuality(int quality) {
+		this.quality = quality;
+	}
+
+	public int getRegion() {
+		return region;
+	}
+
+	public void setRegion(int region) {
+		this.region = region;
+	}
+
+	public int getTaste() {
+		return taste;
+	}
+
+	public void setTaste(int taste) {
+		this.taste = taste;
+	}
+
+	public double getAlcohol() {
+		return alcohol;
+	}
+
+	public void setAlcohol(double alcohol) {
+		this.alcohol = alcohol;
+	}
+
+	public double getAcid() {
+		return acid;
+	}
+
+	public void setAcid(double acid) {
+		this.acid = acid;
+	}
+
+	public double getSweetness() {
+		return sweetness;
+	}
+
+	public void setSweetness(double sweetness) {
+		this.sweetness = sweetness;
+	}
+
+	public int getVdp() {
+		return vdp;
+	}
+
+	public void setVdp(int vdp) {
+		this.vdp = vdp;
+	}
+
+	public int[] getGrape() {
+		return grape;
+	}
+
+	public void setGrape(int[] grape) {
+		this.grape = grape;
+	}
+
+	public int getWineStyle() {
+		return wineStyle;
+	}
+
+	public void setWineStyle(int wineStyle) {
+		this.wineStyle = wineStyle;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+}
