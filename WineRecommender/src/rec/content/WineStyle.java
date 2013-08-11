310bf817355d584e7a97a860657063a88b4dd503
Zolitschka <zolitschka@googlemail.com>
2013-08-11 14:57:51 +0200

    Attribut WineStil hinzugefüg

--- WineRecommender/src/rec/content/WineStyle.java
+++ WineRecommender/src/rec/content/WineStyle.java
@@ -2,7 +2,7 @@ package rec.content;
 
 //Weinstil-Objekt mit Verhältnissen zu allen anderen 8 Weinstilen und Vergleichsmethode
 public class WineStyle {
-	final private int id;
+	final private int wineStyle;
 	final private double relationToWW1;
 	final private double relationToWW2;
 	final private double relationToWW3;
@@ -12,9 +12,9 @@ public class WineStyle {
 	final private double relationToRW3;
 	final private double relationToRW4;
 
-	public WineStyle(int id, double ww1, double ww2, double ww3, double ww4,
+	public WineStyle(int wineStyle, double ww1, double ww2, double ww3, double ww4,
 			double rw1, double rw2, double rw3, double rw4) {
-		this.id = id;
+		this.wineStyle = wineStyle;
 		this.relationToWW1 = ww1;
 		this.relationToWW2 = ww2;
 		this.relationToWW3 = ww3;
@@ -25,22 +25,26 @@ public class WineStyle {
 		this.relationToRW4 = rw4;
 	}
 
-	public double compareTo(int compareId) {
-		if (compareId == 357)
+	public double compareTo(int compareWineStyle) {
+		if (compareWineStyle == 357)
 			return relationToWW1;
-		else if (compareId == 358)
+		else if (compareWineStyle == 358)
 			return relationToWW2;
-		else if (compareId == 359)
+		else if (compareWineStyle == 359)
 			return relationToWW3;
-		else if (compareId == 360)
+		else if (compareWineStyle == 360)
 			return relationToWW4;
-		else if (compareId == 361)
+		else if (compareWineStyle == 361)
 			return relationToRW1;
-		else if (compareId == 362)
+		else if (compareWineStyle == 362)
 			return relationToRW2;
-		else if (compareId == 363)
+		else if (compareWineStyle == 363)
 			return relationToRW3;
 		else
 			return relationToRW4;
+	}
+
+	public int getWineStyle() {
+		return wineStyle;
 	}
 }
