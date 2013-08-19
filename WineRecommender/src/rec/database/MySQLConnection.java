5ce62cf05aa5be5a3393c362abe9ece4767958f2
mapf0000 <maximilian.pfister90@gmail.com>
2013-08-20 00:01:43 +0200

    SQL: WineContent-Abfrage hinzugefügt

--- WineRecommender/src/rec/database/MySQLConnection.java
+++ WineRecommender/src/rec/database/MySQLConnection.java
@@ -149,4 +149,76 @@ public class MySQLConnection {
 		}
 	}
 
+	public static void getWineContent() // TODO return typ anpassen
+	{
+		conn = getInstance();
+
+		if (conn != null) {
+			// Anfrage-Statement erzeugen.
+			Statement query;
+			try {
+				query = conn.createStatement();
+
+				// Ergebnistabelle erzeugen und abholen.
+				String sql = "SELECT pd.entity_id, name, price, manufacturer, manufacturer_value, vdp, taste, taste_value, grape, at.attribute_id, at.value "
+						+ "FROM catalog_product_flat_1 AS pd "
+						+ "INNER JOIN (SELECT entity_id, attribute_id, value "
+						+ "FROM catalog_product_entity_varchar "
+						+ "WHERE attribute_id = 140 OR attribute_id = 141 OR attribute_id = 142 "
+						+ "UNION SELECT entity_id, attribute_id, value "
+						+ "FROM catalog_product_entity_int "
+						+ "WHERE attribute_id = 135 OR attribute_id = 138 OR attribute_id = 166 OR attribute_id = 203) AS at ON pd.entity_id = at.entity_id "
+						+ "ORDER BY pd.entity_id, at.attribute_id";
+				ResultSet result = query.executeQuery(sql);
+
+				// Ergebnissätze durchfahren.
+				while (result.next()) {
+					// TODO Datenstruktur ergänzen
+					int wineID = result.getInt("pd.entity_id");
+					String name = result.getString("name");
+					double price = result.getDouble("price");
+					int manufacturerID = result.getInt("manufacturer");
+					String manufacturer = result.getString("manufacturer_value");
+					boolean vdp = result.getBoolean("vdp");
+					int tasteID = result.getInt("taste");
+					String taste = result.getString("taste_value");
+					// int grape = result.getInt("grape"); //funktioniert noch
+					// nicht weil mehrere Werte in der Spalte vorkommen
+					// können...
+					switch (result.getInt("at.attribute_id")) {
+					case 135:
+						int qualityID = result.getInt("value");
+						break;
+					case 138:
+						int regionID = result.getInt("value");
+						break;
+					case 140:
+						String alcohol = result.getString("value"); // TODO: Datentypumwandlung Problem: Komma und(!) Punkte werden als Trennszeichen für FliessKommazahlen benutzt und Sonderzeichen (z.B "%") kommen vor.
+						break;
+					case 141:
+						String acidity = result.getString("value");// TODO: Datentypumwandlung Problem: Komma und(!) Punkte werden als Trennszeichen für FliessKommazahlen benutzt und Sonderzeichen (z.B "%") kommen vor.
+						break;
+					case 142:
+						String sugar = result.getString("value");// TODO: Datentypumwandlung Problem: Komma und(!) Punkte werden als Trennszeichen für FliessKommazahlen benutzt und Sonderzeichen (z.B "%") kommen vor.
+						break;
+					case 166:
+						int wineStyle = result.getInt("value");
+						break;
+					case 203:
+						int yearID = result.getInt("value");
+					default:
+					}
+					System.out.println("WeinID: " + wineID + " Name: " + name
+							+ " Preis: " + price + " Weingut: " + manufacturer
+							+ " (ID: " + manufacturerID + ") VDP: " + vdp
+							+ "Geschmack: " + taste + "(ID: " + tasteID + ")"); // Für
+																				// Tests
+				}
+				// TODO Datenstruktur returnen
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
