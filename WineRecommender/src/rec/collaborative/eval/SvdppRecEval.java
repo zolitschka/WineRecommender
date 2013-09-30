a0b7424c47d41a5caeae445fcc079d143f91e7f3
mapf0000 <maximilian.pfister90@gmail.com>
2013-09-30 22:06:08 +0200

    test

--- WineRecommender/src/rec/collaborative/eval/SvdppRecEval.java
+++ WineRecommender/src/rec/collaborative/eval/SvdppRecEval.java
@@ -13,15 +13,7 @@ import org.apache.mahout.cf.taste.model.PreferenceArray;
 import org.apache.mahout.cf.taste.recommender.Recommender;
 
 public class SvdppRecEval {
-
-	RecommenderBuilder recBuilder = new RecommenderBuilder() {
-		@Override
-		public Recommender buildRecommender(DataModel dataModel)
-				throws TasteException {
-			return new SVDRecommender(dataModel, new SVDPlusPlusFactorizer(
-					dataModel, 20, 40));
-		}
-	};
+	final int WDH = 1000; //Anzahl der Wiederholungen pro Iterationswert
 	
 	DataModel dataModel = rec.database.MySQLConnection.getDatamodellFromDatabase();
 	
@@ -30,11 +22,29 @@ public class SvdppRecEval {
 		evaluator.setMaxPreference(5);
 		evaluator.setMinPreference(1);
 		try {
-			System.out.println(evaluator.evaluate(recBuilder, null, dataModel, 0.7, 1.0));
+			double rmse = 0;
+			for(int i=0; i< WDH; i++){
+				rmse = rmse + evaluator.evaluate(createRecBuilder(20, 20), null, dataModel, 0.7, 1.0);
+			}
+			System.out.println(rmse / WDH);
 		} catch (TasteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public RecommenderBuilder createRecBuilder(int factors, int iterations){
+		//Konstanten für den Zugriff aus der anonymen Klasse
+		final int fac = factors;
+		final int iter = iterations;
+		return new RecommenderBuilder() {
+			@Override
+			public Recommender buildRecommender(DataModel dataModel)
+					throws TasteException {
+				return new SVDRecommender(dataModel, new SVDPlusPlusFactorizer(
+						dataModel, fac, iter));
+			}
+		};
 	}
 
 }
--- WineRecommender/src/rec/main.java
+++ WineRecommender/src/rec/main.java
@@ -2,9 +2,13 @@ package rec;
 
 import org.apache.mahout.cf.taste.common.TasteException;
 
+import rec.collaborative.eval.SvdppRecEval;
+
 public class main {
 	// GUI skalierbar �ber Parameter im Konstruktor
 	public static void main(String[] args) throws TasteException {
 		new GUI(800);
+//		SvdppRecEval evaluator = new SvdppRecEval();
+//		evaluator.eval();
 	}
 }
